2f16204abfd2adc232972a57db07e328d9f72310
Tim <tim.maastricht@gmail.com>
2026-04-23 18:10:02 +1000

    feat: add more cards

---
+++ src/components/RandomPokemonCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default class RandomPokemonCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pokemonName: "",
+      pokemonSprite: "",
+    };
+  }
+
+  async componentDidMount(){
+    // generate a random pokemon
+    let randomPokemonNumber = Math.floor(Math.random() * 1025) + 1;
+
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomPokemonNumber);
+    let jsonData = await response.json();
+
+    this.setState({
+      pokemonName: jsonData.name,
+      pokemonSprite: jsonData.sprites.front_default
+    })
+  }
+
+  render() {
+    return (
+      <div className="randomPokemonCard">
+        <h1>{this.state.pokemonName}</h1>
+        <img
+          src={this.state.pokemonSprite}
+          alt={"sprite for pokemon named " + this.state.pokemonName}
+        />
+      </div>
+    );
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,21 @@ import './styles/index.css'
 import App from './components/App.jsx'
 import PageLayout from './components/_PageLayout.jsx'
 import ButtonCounter from './components/ButtonCounter.jsx'
+import RandomPokemonCard from './components/RandomPokemonCard.jsx'
 // add more components
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PageLayout>
-      <ButtonCounter />
-      <FavPokemonList />
+      <RandomPokemonCard />
+      <RandomPokemonCard />
+      <RandomPokemonCard />
+      <RandomPokemonCard />
+      <RandomPokemonCard />
+      <RandomPokemonCard />
+      <RandomPokemonCard />
+      <RandomPokemonCard />
+      {/* <ButtonCounter /> */}
+      {/* <FavPokemonList /> */}
     </PageLayout>
     
     {/* <App /> */}
